dbbbf38bef632c5fe71614d5c89dafa0f75975cb
h-otter <h-otter@outlook.jp>
2017-05-12 00:52:26 +0900

    remove fact class on models.py

--- mike/lib/objects/switches.py
+++ mike/lib/objects/switches.py
@@ -1,5 +1,31 @@
-from mike.models import ModelSwitch
 from mike.lib.objects.n0stack_object import N0stackObject
+
+from django.db import models
+from uuid import uuid4
+
+
+class ModelSwitch(models.Model):
+    '''
+    {
+        id: UUID,
+        name: string,
+        host_id: reference uuid,
+        internal: boolean,
+        datapath_id: integer,
+    }
+    '''
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=32, null=False)
+    host_id = models.UUIDField(null=False, editable=False)
+    internal = models.BooleanField(null=False, editable=False, default=True)
+    datapath_id = models.IntegerField(null=False)  # editable?
+
+    class Meta:
+        unique_together = (('host_id', 'datapath_id'))
+        app_label = 'mike'
+
+    def __unicode__(self):
+        return self.uuid
 
 
 class Switches(N0stackObject):
