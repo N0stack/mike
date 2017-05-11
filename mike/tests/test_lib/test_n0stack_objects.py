bad9a707eb58066565486ad1de669907d379b252
h-otter <h-otter@outlook.jp>
2017-05-12 01:27:57 +0900

    test methods to class

--- mike/tests/test_lib/test_n0stack_objects.py
+++ mike/tests/test_lib/test_n0stack_objects.py
@@ -7,44 +7,44 @@ from django.db.utils import IntegrityError
 from uuid import uuid4
 
 
-def test_switches():
-    host_id = uuid4()
+class TestSwitches(TestCase):
+    def test_switches(self):
+        host_id = uuid4()
 
-    uuids = []
-    uuids.append(Switches.add(name="test_sw1",
-                              host_id=host_id,
-                              datapath_id=1))
-    r = Switches.get_from_uuids(uuids)[0]
-    eq_(r.name, "test_sw1")
-    eq_(r.host_id, host_id)
-    ok_(r.internal)
-    eq_(r.datapath_id, 1)
+        uuids = []
+        uuids.append(Switches.add(name="test_sw1",
+                                  host_id=host_id,
+                                  datapath_id=1))
+        r = Switches.get_from_uuids(uuids)[0]
+        eq_(r.name, "test_sw1")
+        eq_(r.host_id, host_id)
+        ok_(r.internal)
+        eq_(r.datapath_id, 1)
 
-    uuids.append(Switches.add(name="test_sw2",
-                              host_id=host_id,
-                              internal=False,
-                              datapath_id=2))
-    eq_(len(Switches.get_from_uuids(uuids)), 2)
-    Switches.delete([uuids[0]])
+        uuids.append(Switches.add(name="test_sw2",
+                                  host_id=host_id,
+                                  internal=False,
+                                  datapath_id=2))
+        eq_(len(Switches.get_from_uuids(uuids)), 2)
+        Switches.delete([uuids[0]])
 
-    r = Switches.get_from_uuids(uuids)[0]
-    eq_(r.name, "test_sw2")
-    eq_(r.host_id, host_id)
-    ok_(not r.internal)
-    eq_(r.datapath_id, 2)
+        r = Switches.get_from_uuids(uuids)[0]
+        eq_(r.name, "test_sw2")
+        eq_(r.host_id, host_id)
+        ok_(not r.internal)
+        eq_(r.datapath_id, 2)
 
+    @raises(IntegrityError)
+    def test_add_exists_switches(self):
+        host_id = uuid4()
 
-@raises(IntegrityError)
-def test_add_exists_switches():
-    host_id = uuid4()
-
-    uuids = []
-    uuids.append(Switches.add(name="test_sw1",
-                              host_id=host_id,
-                              datapath_id=1))
-    uuids.append(Switches.add(name="test_sw1",
-                              host_id=host_id,
-                              datapath_id=1))
+        uuids = []
+        uuids.append(Switches.add(name="test_sw1",
+                                  host_id=host_id,
+                                  datapath_id=1))
+        uuids.append(Switches.add(name="test_sw1",
+                                  host_id=host_id,
+                                  datapath_id=1))
 
 
 class TestPorts(TestCase):
@@ -140,6 +140,9 @@ class TestPorts(TestCase):
         eq_(p.mac_addr, "10:00:00:00:00:04")
         eq_(p.ipv4_addr, "192.168.0.1")
         eq_(p.ipv4_subnet_mask, "255.255.255.0")
+
+        # __unicode__ との比較がうまく行かないので迷う
+        # eq_(p, ports_uuids[5])
 
         eq_(len(Ports.get_from_uuids(ports_uuids)), 6)
         del_p = Ports.model.objects.filter(switch=self.switch)
