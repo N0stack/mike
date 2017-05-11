6c27c3f3222f59c29d56318cafdb1b5a3a015723
h-otter <h-otter@outlook.jp>
2017-05-12 00:48:28 +0900

    add test for Ports

--- mike/test/test_lib/test_n0stack_objects.py
+++ mike/test/test_lib/test_n0stack_objects.py
@@ -1,5 +1,7 @@
 from mike.lib.objects.switches import Switches
+from mike.lib.objects.ports import Ports
 
+from unittest import TestCase
 from nose.tools import eq_, ok_, raises
 from django.db.utils import IntegrityError
 from uuid import uuid4
@@ -43,3 +45,144 @@ def test_add_exists_switches():
     uuids.append(Switches.add(name="test_sw1",
                               host_id=host_id,
                               datapath_id=1))
+
+
+class TestPorts(TestCase):
+    def setUp(self):
+        self.host_id = uuid4()
+        self.switch_uuid = Switches.add(name="test_sw1",
+                                        host_id=self.host_id,
+                                        datapath_id=1)
+        self.switch = Switches.get_from_uuids([self.switch_uuid])[0]
+
+    def test_ports(self):
+        ports_uuids = []
+
+        # no name port
+        ports_uuids.append(Ports.add(number=1,
+                                     switch=self.switch,
+                                     mac_addr="10:00:00:00:00:01"))
+        # no ip address port
+        ports_uuids.append(Ports.add(number=2,
+                                     name="veth0",
+                                     switch=self.switch,
+                                     mac_addr="10:00:00:00:00:02"))
+        # same mac address port
+        ports_uuids.append(Ports.add(number=3,
+                                     name="veth1",
+                                     switch=self.switch,
+                                     mac_addr="10:00:00:00:00:02"))
+        # ip address port
+        ports_uuids.append(Ports.add(number=4,
+                                     name="veth2",
+                                     switch=self.switch,
+                                     mac_addr="10:00:00:00:00:03",
+                                     ipv4_addr="192.168.0.1",
+                                     ipv4_subnet_mask="255.255.255.0"))
+        # same ip address port
+        ports_uuids.append(Ports.add(number=5,
+                                     name="veth3",
+                                     switch=self.switch,
+                                     mac_addr="10:00:00:00:00:04",
+                                     ipv4_addr="192.168.0.1",
+                                     ipv4_subnet_mask="255.255.255.0"))
+        sw2_uuid = Switches.add(name="test_sw2",
+                                host_id=self.host_id,
+                                datapath_id=21)
+        sw2 = Switches.get_from_uuids([sw2_uuid])[0]
+        # othter switch same config
+        ports_uuids.append(Ports.add(number=5,
+                                     name="veth3",
+                                     switch=sw2,
+                                     mac_addr="10:00:00:00:00:04",
+                                     ipv4_addr="192.168.0.1",
+                                     ipv4_subnet_mask="255.255.255.0"))
+
+        p = Ports.get_from_uuids([ports_uuids[0]])[0]
+        eq_(p.number, 1)
+        eq_(p.name, "")
+        eq_(p.switch, self.switch)
+        eq_(p.mac_addr, "10:00:00:00:00:01")
+        eq_(p.ipv4_addr, "")
+        eq_(p.ipv4_subnet_mask, "")
+        p = Ports.get_from_uuids([ports_uuids[1]])[0]
+        eq_(p.number, 2)
+        eq_(p.name, "veth0")
+        eq_(p.switch, self.switch)
+        eq_(p.mac_addr, "10:00:00:00:00:02")
+        eq_(p.ipv4_addr, "")
+        eq_(p.ipv4_subnet_mask, "")
+        p = Ports.get_from_uuids([ports_uuids[2]])[0]
+        eq_(p.number, 3)
+        eq_(p.name, "veth1")
+        eq_(p.switch, self.switch)
+        eq_(p.mac_addr, "10:00:00:00:00:02")
+        eq_(p.ipv4_addr, "")
+        eq_(p.ipv4_subnet_mask, "")
+        p = Ports.get_from_uuids([ports_uuids[3]])[0]
+        eq_(p.number, 4)
+        eq_(p.name, "veth2")
+        eq_(p.switch, self.switch)
+        eq_(p.mac_addr, "10:00:00:00:00:03")
+        eq_(p.ipv4_addr, "192.168.0.1")
+        eq_(p.ipv4_subnet_mask, "255.255.255.0")
+        p = Ports.get_from_uuids([ports_uuids[4]])[0]
+        eq_(p.number, 5)
+        eq_(p.name, "veth3")
+        eq_(p.switch, self.switch)
+        eq_(p.mac_addr, "10:00:00:00:00:04")
+        eq_(p.ipv4_addr, "192.168.0.1")
+        eq_(p.ipv4_subnet_mask, "255.255.255.0")
+        p = Ports.get_from_uuids([ports_uuids[5]])[0]
+        eq_(p.number, 5)
+        eq_(p.name, "veth3")
+        eq_(p.switch, sw2)
+        eq_(p.mac_addr, "10:00:00:00:00:04")
+        eq_(p.ipv4_addr, "192.168.0.1")
+        eq_(p.ipv4_subnet_mask, "255.255.255.0")
+
+        eq_(len(Ports.get_from_uuids(ports_uuids)), 6)
+        del_p = Ports.model.objects.filter(switch=self.switch)
+        eq_(len(del_p), 5)
+        Ports.delete(del_p)
+        eq_(len(Ports.get_from_uuids(ports_uuids)), 1)
+
+
+    @raises(IntegrityError)
+    def test_add_exists_ports(self):
+        Ports.add(number=1,
+                  switch=self.switch,
+                  mac_addr="10:00:00:00:00:01")
+        Ports.add(number=1,
+                  switch=self.switch,
+                  mac_addr="10:00:00:00:00:01")
+
+    @raises(Exception)
+    def test_add_ports_invalid_args(self):
+        Ports.add(number=1,
+                  name="veth1",
+                  switch=self.switch,
+                  mac_addr="10:00:00:00:00:01",
+                  ipv4_addr="192.168.0.1")
+
+    @raises(Exception)
+    def test_add_ports_invalid_args_no_name(self):
+        Ports.add(number=1,
+                  switch=self.switch,
+                  mac_addr="10:00:00:00:00:01",
+                  ipv4_addr="192.168.0.1")
+
+    @raises(Exception)
+    def test_add_ports_invalid_args_subnet(self):
+        Ports.add(number=1,
+                  name="veth1",
+                  switch=self.switch,
+                  mac_addr="10:00:00:00:00:01",
+                  ipv4_subnet_mask="255.255.255.0")
+
+    @raises(Exception)
+    def test_add_ports_invalid_args_no_name_subnet(self):
+        Ports.add(number=1,
+                  switch=self.switch,
+                  mac_addr="10:00:00:00:00:01",
+                  ipv4_subnet_mask="255.255.255.0")
