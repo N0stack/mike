92f9e06d91bf51f8f476ac85e634a48592f138c7
h-otter <h-otter@outlook.jp>
2017-06-01 14:09:42 +0900

    remove hw_addr from port

--- mike/lib/objects/port.py
+++ mike/lib/objects/port.py
@@ -11,18 +11,6 @@ class Port(Interface):
         "name": string,
         "number": integer,
         "switch": reference,
-        "hw_addrs": [
-            char[8],
-        ]
     }
     '''
     switch = models.ForeignKey(Switch, related_name="ports", null=False, editable=False)
-
-
-class PortHwaddr(models.Model):
-    port = models.ForeignKey(Port, related_name="hw_addrs", null=False, editable=False)
-    hw_addr = models.CharField(max_length=12, null=False)  # ie. 1a2b3c4d5e6f
-
-    class Meta:
-        unique_together = (('port', 'hw_addr'))
-
