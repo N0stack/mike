f13f0e40b037351025ad6218b6f6dd67b29334c1
h-otter <h-otter@outlook.jp>
2017-06-03 00:33:33 +0900

    load django settings on only ryu-manager

--- mike/controller/__init__.py
+++ mike/controller/__init__.py
@@ -1,5 +1,8 @@
-import os
-import django
+import inspect
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mike.settings")
-django.setup()
+if 'ryu' in inspect.stack()[-1].filename:
+    import os
+    import django
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mike.settings")
+    django.setup()
